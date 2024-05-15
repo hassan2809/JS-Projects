e7cc52605ac7fab9ada21e375729273808fbcaea
Hassan Mehmood <chaudahryhassan786@gmail.com>
2024-05-15 18:06:24 +0500

    Add files via upload

---
+++ Car_Racing_Game/script.js
@@ -0,0 +1,4 @@
+let x=document.createElement("Audio")
+x.setAttribute('src','/Images/sound.mp3')
+x.loop=true
+x.play()
---
+++ Dino_Game/script.js
@@ -0,0 +1,5 @@
+console.log(1)
+document.onkeydown=function(e){
+    console.log("Key code is: ",e.code)
+    console.log("Key code is: ",e.key)
+}
